30274013fe5ae15c0f77eb36337c1d0585a3ce74
Kilian CUNY <6116840+Qu1lby@users.noreply.github.com>
2021-09-29 12:02:11 +0200

    Change passURL from paymentPass to pass

--- src/ios/CDVAppleWallet.m
+++ src/ios/CDVAppleWallet.m
@@ -652,7 +652,7 @@ typedef void (^completedPaymentProcessHandler)(PKAddPaymentPassRequest *request)
       for (PKPass *pass in paymentPasses) {
         PKSecureElementPass *paymentPass = [pass secureElementPass];
         if ([[paymentPass primaryAccountNumberSuffix] isEqualToString:cardSuffix]) {
-            [[UIApplication sharedApplication] openURL:[paymentPass passURL] options:@{} completionHandler:nil];
+            [[UIApplication sharedApplication] openURL:[pass passURL] options:@{} completionHandler:nil];
         }
       }
     } else {
@@ -660,7 +660,7 @@ typedef void (^completedPaymentProcessHandler)(PKAddPaymentPassRequest *request)
       for (PKPass *pass in paymentPasses) {
         PKPaymentPass *paymentPass = [pass paymentPass];
         if([[paymentPass primaryAccountNumberSuffix] isEqualToString:cardSuffix]) {
-            [[UIApplication sharedApplication] openURL:[paymentPass passURL] options:@{} completionHandler:nil];
+            [[UIApplication sharedApplication] openURL:[pass passURL] options:@{} completionHandler:nil];
         }
       }
     }
